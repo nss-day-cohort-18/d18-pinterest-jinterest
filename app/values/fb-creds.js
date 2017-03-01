8629d29359e9e36488f323471280fb0cb2cbb29b
Jordan Nelson <jnelsontn@users.noreply.github.com>
2017-03-01 10:07:18 -0600

    Delete fb-creds.js

--- app/values/fb-creds.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-app.constant('FBCreds', 
-{
-	apiKey: 'AIzaSyBA6DaKdfb9Q4u_BOAUnY4J7qU3QUlM2QI',
-	authDomain: 'jinterest-4e024.firebaseapp.com',
-	databaseURL: 'https://jinterest-4e024.firebaseio.com',
-});
