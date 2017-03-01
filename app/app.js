60e9d4c5f841c63de9d7564c6d87b3fd42e95333
Jordan Nelson <jnelsontn@gmail.com>
2017-03-01 09:52:23 -0600

    boilerplate

--- app/app.js
+++ app/app.js
@@ -17,7 +17,41 @@ let isAuth = (AuthFactory) => new Promise ( (resolve, reject) => {
     });
 });
 
-
+app.config(function($routeProvider) {
+    $routeProvider.
+    when('/', {
+        templateUrl: 'template/login.html',
+        controller: 'UserCtrl'
+    }).
+    when('/login', {
+        templateUrl: 'template/login.html',
+        controller: 'UserCtrl'
+/*)    }).
+    when('/logout', {
+        templateUrl: 'template/login.html',
+        controller: 'UserCtrl'
+    }).
+    when('/music/list', {
+        templateUrl: 'template/SongList-template.html',
+        controller: 'SongListCtrl',
+        resolve: {isAuth}
+    }).
+    when('/music/addSong', {
+        templateUrl: 'template/SongForm-template.html',
+        controller: 'SongAddCtrl',
+        resolve: {isAuth}
+    }).
+    when('/music/:songId', {
+        templateUrl: 'template/SongDetail-template.html',
+        controller: 'SongDetailCtrl',
+        resolve: {isAuth}
+    }).
+    when('/music/:songId/edit', {
+        templateUrl: 'template/SongForm-template.html',
+        controller: 'SongEditCtrl',
+        resolve: {isAuth}
+    }); */
+});
 
 // Start Firebase Credentials
 app.run(($location, FBCreds) => {
--- app/controllers/userCtrl.js
+++ app/controllers/userCtrl.js
@@ -1,6 +1,70 @@
-"use strict";
+'use strict';
 
-app.controller('userCtrl', function($scope, $window, AuthFactory) {
+// login, logout, register, loginGoogle, conditional, authfactory
+app.controller('userCtrl', function ($scope, $window, AuthFactory) {
+
+/*
+	$location does not cause a full page reload when the browser URL is changed. 
+	To reload the page after changing the URL, use the lower-level API, $window.location.href.
+	http://docs.angularjs.org/guide/dev_guide.services.$location
+ */
+	$scope.isLoggedIn = false;
+
+	$scope.account = {
+		email: '',
+		password: ''
+	};
+
+	$scope.logout = () => {
+		console.log("logout clicked");
+		AuthFactory.logoutUser().then(function(data){
+			console.log("logged out?", data);
+			$window.location.url = "#!/login";
+			$scope.isLoggedIn = false;
+		}, function(error){
+			console.log("error occured on logout");
+		});
+	};
+
+	//when first loaded, make sure no one is logged in
+	if(AuthFactory.isAuthenticated()){
+		$scope.logout();
+	}
+
+	$scope.register = () => {
+    	console.log("you clicked register");
+	    AuthFactory.createUser({
+	      email: $scope.account.email,
+	      password: $scope.account.password
+	    }).then((userData) => {
+	      console.log("UserCtrl newUser: ", userData );
+	      $scope.login();
+	    }, (error) => {
+	        console.log("Error creating user: ", error);
+	    });
+  	};
+
+  	$scope.login = () => {
+    	console.log("you clicked login");
+    	AuthFactory.loginUser($scope.account)
+	    .then( () => {
+	    	console.log("UserCtrl: user is loggedIn", $scope.isLoggedIn );
+	        $scope.isLoggedIn = true;
+	        $window.location.href = "#!/music/list";
+	    });
+	};
+
+	$scope.loginGoogle = () => {
+		console.log("you clicked login with Google");
+		AuthFactory.authWithProvider().then(function(result) {
+	    	console.log("logged in user: ", result.user.uid);
+	    	//Once logged in, go to another view
+	    	$scope.isLoggedIn = true;
+	    	$window.location.href = "#!/music/list";
+	  	}).catch(function(error) {
+	    	console.log("error with google login", error);
+	  	});
+	};
 
 
 });
