e6339e18ea8d3fc52aac23cddfbe9c4539f7ed4b
Kayla Brewer <cooper.kaylam@gmail.com>
2017-03-01 14:30:53 -0600

    Added Firebase factory. Currently has calls to get all jins, get user jins, add new jin, and add new board

---
+++ app/factories/FirebaseFactory.js
@@ -0,0 +1,70 @@
+"use strict";
+
+app.factory("FirebaseStorage", function(FBCreds, $q, $http, AuthFactory) {
+
+    let getAllJins = () => {
+        let jins = [];
+        return $q((resolve, reject) => {
+            $http.get(`${FBCreds.databaseURL}/pins.json`)
+            .then((jinObject) =>{
+                let jinCollection = jinObject.data;
+                Object.keys(jinCollection).forEach((key)=>{
+                    jinCollection[key].id = key;
+                    jins.push(jinCollection[key]);
+                });
+                resolve(jins);
+            })
+            .catch((error)=> {
+                reject(error);
+            });
+        });
+    };
+
+    let getUserJins = (user) => {
+        let jins = [];
+        return $q((resolve, reject) => {
+            $http.get(`${FBCreds.databaseURL}/pins.json?orderBy="uid"&equalTo="${user}"`)
+            .then((jinObject) =>{
+                let jinCollection = jinObject.data;
+                Object.keys(jinCollection).forEach((key)=>{
+                    jinCollection[key].id = key;
+                    jins.push(jinCollection[key]);
+                });
+                resolve(jins);
+            })
+            .catch((error)=> {
+                reject(error);
+            });
+        });
+    };
+
+
+    let addNewJin = (newJin) => {
+        return $q((resolve, reject)=>{
+            $http.post(`${FBCreds.databaseURL}/pins.json`,
+                JSON.stringify(newJin))
+                .then((ObjectFromFirebase) => {
+                    resolve(ObjectFromFirebase);
+                })
+                .catch((error)=>{
+                    reject(error);
+                });
+            });
+    };
+
+    let addNewBoard = (newBoard) => {
+        return $q((resolve, reject)=>{
+            $http.post(`${FBCreds.databaseURL}/bords.json`,
+                JSON.stringify(newBoard))
+                .then((ObjectFromFirebase) => {
+                    resolve(ObjectFromFirebase);
+                })
+                .catch((error)=>{
+                    reject(error);
+                });
+            });
+    };
+
+    return {getAllJins, getUserJins, addNewJin, addNewBoard};
+
+});
