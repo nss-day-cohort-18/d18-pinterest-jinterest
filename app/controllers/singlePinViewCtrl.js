5d687a5278f1c1dbfeb69445a93f385498bb0357
Jordan Nelson <jnelsontn@gmail.com>
2017-03-02 12:22:21 -0600

    misc change

---
+++ app/controllers/singlePinViewCtrl.js
@@ -0,0 +1,18 @@
+'use strict';
+
+app.controller('singlePinViewCtrl', function($scope, AuthFactory, FirebaseStorage) {
+
+// get single pin - getSingleJin - call 
+//FirebaseStorage.getSingleJin
+// FirebaseStorage.getUserBoards
+// get user's boards - getUserBoards - call
+
+// copy get single pin - {{board.id}}
+
+
+// addPinToBoard call - addNewJin
+	// we're creating a new 
+
+
+
+});
