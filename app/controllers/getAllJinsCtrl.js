2e7801b698c68e83331315bba6fdced9af8a1cbd
Kayla Brewer <cooper.kaylam@gmail.com>
2017-03-03 11:38:16 -0600

    added filter for view all Jins

--- app/controllers/getAllJinsCtrl.js
+++ app/controllers/getAllJinsCtrl.js
@@ -1,10 +1,12 @@
 'use strict';
 
 app.controller('getAllJinsCtrl', function($scope, $routeParams, $location, AuthFactory, FirebaseStorage) {
+$scope.allUserJins = [];
 
     FirebaseStorage.getAllJins().then(function(allJins){
         // console.log('Complete list of all jins: ', allJins);
         $scope.allUserJins = allJins;
+        console.log("things", $scope.allUserJins);
     });
 
 	// console.log('Current location: ' + $location.path());
